70926b27257fb6738147291f77dc39d6f4aca17c
LovelyPenguin <lyp3238@gmail.com>
2019-03-27 00:59:14 +0900

    유니티 기본 프로젝트 생성

---
+++ Assets/Scripts/MenuItemSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuItemSetting : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [MenuItem("MyMenu/Anything")]
+    static void Penguin()
+    {
+        Debug.Log("Hello!");
+    }
+}
